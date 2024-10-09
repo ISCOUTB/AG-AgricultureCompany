1c71fc2df42492634f8f1d066fa4c502d7396690
Nicolás Molina <nicolas04133@gmail.com>
2024-10-09 11:11:39 -0500

    New changes

---
+++ app/app.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# Path to the templates folder
+templates = Jinja2Templates(directory="templates")
+
+
+# Home page - Index
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+# About Us page
+@app.get("/about_us", response_class=HTMLResponse)
+async def about_us(request: Request):
+    return templates.TemplateResponse("about_us.html", {"request": request})
+
+
+# Contact Us page
+@app.get("/contact", response_class=HTMLResponse)
+async def contact_us(request: Request):
+    return templates.TemplateResponse("contact_us.html", {"request": request})
+
+
