4161d367750e1e45c8e46ca7475db967c4848c9e
Miguel Cuentas <159710709+mogi22210@users.noreply.github.com>
2024-11-13 20:24:23 -0500

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -66,10 +66,6 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
-    #connectable = engine_from_config(
-     #   config.get_section(config.config_ini_section, {}),
-      #  prefix="sqlalchemy.",
-       # poolclass=pool.NullPool,
     #)
 
     configuration = config.get_section(config.config_ini_section)
